6edd0841c2231d278b71146c4078ab6ff43aab40
Björn Daigger <124874016+bjoerndaigger@users.noreply.github.com>
2023-09-27 14:22:05 +0200

    collectionData vs onSnapshot

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,9 +1,51 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import {
+  Firestore,
+  collection,
+  doc,
+  collectionData,
+  onSnapshot,
+} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NoteListService {
+  // items$;
+  // items;
+  unsubList;
+  unsubSingle;
 
-  constructor() { }
+
+  firestore: Firestore = inject(Firestore);
+
+  constructor() {
+    this.unsubList = onSnapshot(this.getNotesRef(), (list) => {
+    });
+    this.unsubSingle = onSnapshot(this.getSingleDocRef('notes', 'gk07bXX8xBAVtRgVZwyK'), (list) => {
+    });
+
+    this.unsubList(); // Unsubcribe
+    this.unsubSingle(); // Unsubscribe
+   
+    // this.items$ = collectionData(this.getNotesRef());
+    // this.items = this.items$.subscribe((list) => {
+    //  console.log(list);
+    // });
+    // this.items.unsubscribe();
+  }
+
+  getNotesRef() {
+    return collection(this.firestore, 'notes');
+  }
+
+  getTrashRef() {
+    return collection(this.firestore, 'trash');
+  }
+
+  getSingleDocRef(colId: string, docId: string) {
+    return doc(collection(this.firestore, colId), docId);
+  }
 }
+

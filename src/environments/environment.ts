7ed8ff1d81dd27c59659efe32190752f8eb33247
Björn Daigger <124874016+bjoerndaigger@users.noreply.github.com>
2023-09-27 12:16:29 +0200

    connected firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'danotes-15ac9',
+    appId: '1:254551169923:web:c8ecddf73366f38cd434b6',
+    storageBucket: 'danotes-15ac9.appspot.com',
+    apiKey: 'AIzaSyBc7q8QkT7h68UCTD8PRB_cYpk43Lu8wcU',
+    authDomain: 'danotes-15ac9.firebaseapp.com',
+    messagingSenderId: '254551169923',
+  },
+};
